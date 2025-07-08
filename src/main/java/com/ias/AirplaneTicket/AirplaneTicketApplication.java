f2ed3777a0ff9b66cc8c5d361e82a74c1e885161
NickyFlorezIas <nicky.florez@ias.com.co>
2025-07-08 10:40:19 -0500

    first commit

---
+++ src/main/java/com/ias/AirplaneTicket/AirplaneTicketApplication.java
@@ -0,0 +1,13 @@
+package com.ias.AirplaneTicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirplaneTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirplaneTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ias/AirplaneTicket/AirplaneTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ias.AirplaneTicket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirplaneTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
